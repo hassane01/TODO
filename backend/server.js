2c76dc0c256b7b9c399ac82e7b0db9b1e698b1b3
hassane01 <oubakassehassane7@gmail.com>
2025-12-19 22:20:08 +0100

    Update start script in package.json for production; enhance server startup log message

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.use(errorHandler);
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server now running on port ${PORT}`)
   );
 }
 
